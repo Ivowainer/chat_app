e5f629f72268a178d3100ee59e82f841f8431dd2
Ivowainer <ivancamposwainer@gmail.com>
2022-07-26 17:22:56 -0300

    Getting all chats

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,28 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 const ChatPage = () => {
-  return (
-    <div>ChatPage</div>
-  )
+    const [chats, setChats] = useState([])
+
+    useEffect(() => {
+        const fetchChats = async () => {
+            const { data } = await axios.get(`/api/chats`)
+    
+            setChats(data)
+        }
+
+        fetchChats()
+    }, [])
+
+    return (
+        <div>
+            {chats.map(chat => (
+                <div key={chat._id}>
+                    {chat.chatName}
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default ChatPage
