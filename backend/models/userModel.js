f50fd19bdef9b28889df8394a174ba2caf626f36
Ivowainer <ivancamposwainer@gmail.com>
2022-07-27 08:35:24 -0300

    Creating the User, Chat and Message Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userModel = mongoose.Schema({
+    name: { type: String, required: true, },
+    email: { type: String, required: true },
+    pic: { 
+        type: String, 
+        required: true, 
+        default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" 
+    },
+
+}, {
+    timestamps: true
+})
+
+export const User = mongoose.model("User", userModel)
